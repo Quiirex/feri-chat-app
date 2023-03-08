6630d06110ade6e91532e9f21005574b9b795bef
Luka Mlinarič <j6.bountyhounter@gmail.com>
2023-03-08 10:31:39 +0100

    Small changes

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -14,15 +14,14 @@ function App() {
     return (
         <div className="App">
             <div>
-                <a href="https://github.com/electron-vite/electron-vite-react" target="_blank">
+                {/*<a href="https://github.com/electron-vite/electron-vite-react" target="_blank">
                     <img src="./electron-vite.svg" className="logo" alt="Electron + Vite logo" />
-                </a>
+                </a>*/}
             </div>
             <div className="card">
                 {/* <button onClick={() => setCount((count) => count + 1)}>
                     count is {count}
                 </button> */}
-                <p>Demo (sign in with google doesn't work) in desktop version</p>
                 {<Chat />}
                 {/* <p>
                     Edit <code>src/App.tsx</code> and save to test HMR
