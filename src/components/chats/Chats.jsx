d0d2ddc08edebf637256ffc0440c7dfc5a6ed3cf
Luka Mlinarič <j6.bountyhounter@gmail.com>
2023-05-04 22:43:07 +0200

    Add chats component

---
+++ src/components/chats/Chats.jsx
@@ -0,0 +1,18 @@
+const Chats = () => {
+  return (
+    <div className="chats">
+      <div className="userChat">
+        <img
+          src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__340.png"
+          alt=""
+        />
+        <div className="userChatInfo">
+          <span>User display name</span>
+          <p>Chat message</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Chats;
