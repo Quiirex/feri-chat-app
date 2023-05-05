b185d1cd813cbe55b79f33fffaeb9e1fabb4a21e
AmbicatusThe2nd <rok99tkalcic@gmail.com>
2023-05-05 14:57:28 +0200

    Added simple function for navigation to home page

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,10 +1,20 @@
+import { useNavigate } from "react-router-dom";
+
 const Login = () => {
+
+    const navigate = useNavigate();
+
+    const handleLogin = (e) => {
+        e.preventDefault();
+        navigate('/');
+    }
+
     return (
         <div className="login">
             <div className="formContainer">
                 <div className="formWrapper">
                     <span className="logo"> FERI Chat </span>
-                    <form>
+                    <form onSubmit={handleLogin}>
                         <input className="email" placeholder="Email" type="email" />
                         <input className="password" placeholder="Password" type="password" />
                         <button className="loginButton">Login</button>
