acdbc3dc65127d9712a90c67729a765d068d09a7
AmbicatusThe2nd <rok99tkalcic@gmail.com>
2023-05-06 18:59:52 +0200

    Added useState on registration form

--- src/components/register/Register.jsx
+++ src/components/register/Register.jsx
@@ -1,7 +1,22 @@
 import { Link, useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 
 const Register = () => {
   const navigate = useNavigate();
+
+  const [inputs, setInputs] = useState({
+    firstname: '',
+    lastname: '',
+    email: '',
+    password: '',
+    passwordAgain: '',
+  });
+
+  const handleChange = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setInputs({ ...inputs, [name]: value });
+  };
 
   const handleRegister = (e) => {
     e.preventDefault();
@@ -14,16 +29,43 @@ const Register = () => {
         <div className="formWrapper">
           <span className="logo"> FERI Chat </span>
           <form onSubmit={handleRegister}>
-            <input className="firstname" placeholder="First name" type="text" />
-            <input className="lastname" placeholder="Last name" type="text" />
-            <input className="email" placeholder="Email" type="email" />
+            <input
+              className="firstname"
+              name="firstname"
+              value={inputs.firstname}
+              onChange={handleChange}
+              placeholder="First name"
+              type="text"
+            />
+            <input
+              className="lastname"
+              name="lastname"
+              value={inputs.lastname}
+              onChange={handleChange}
+              placeholder="Last name"
+              type="text"
+            />
+            <input
+              className="email"
+              name="email"
+              value={inputs.email}
+              onChange={handleChange}
+              placeholder="Email"
+              type="email"
+            />
             <input
               className="password"
+              name="password"
+              value={inputs.password}
+              onChange={handleChange}
               placeholder="Password"
               type="password"
             />
             <input
               className="passwordAgain"
+              name="passwordAgain"
+              value={inputs.passwordAgain}
+              onChange={handleChange}
               placeholder="Repeat Password"
               type="password"
             />
