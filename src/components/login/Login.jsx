fcbe1483af303e3f1e57d992b4e98a1a54db31b6
AmbicatusThe2nd <rok99tkalcic@gmail.com>
2023-05-06 15:18:07 +0200

    Formating

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,31 +1,37 @@
-import { useNavigate, Link } from "react-router-dom";
+import { useNavigate, Link } from 'react-router-dom';
 
 const Login = () => {
+  const navigate = useNavigate();
 
-    const navigate = useNavigate();
+  const handleLogin = (e) => {
+    e.preventDefault();
+    navigate('/');
+  };
 
-    const handleLogin = (e) => {
-        e.preventDefault();
-        navigate('/');
-    }
-
-    return (
-        <div className="login">
-            <div className="formContainer">
-                <div className="formWrapper">
-                    <span className="logo"> FERI Chat </span>
-                    <form onSubmit={handleLogin}>
-                        <input className="email" placeholder="Email" type="email" />
-                        <input className="password" placeholder="Password" type="password" />
-                        <button className="loginButton">Login</button>
-                    </form>
-                    <p> 
-                        Don't have an account? <Link to="/register">Register</Link>
-                     </p>
-                </div>
-            </div>
+  return (
+    <div className="login">
+      <div className="formContainer">
+        <div className="formWrapper">
+          <span className="logo"> FERI Chat </span>
+          <form onSubmit={handleLogin}>
+            <input className="email" placeholder="Email" type="email" />
+            <input
+              className="password"
+              placeholder="Password"
+              type="password"
+            />
+            <button className="loginButton">Login</button>
+          </form>
+          <p>
+            Don't have an account?{' '}
+            <Link className="link" to="/register">
+              Register
+            </Link>
+          </p>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 export default Login;
