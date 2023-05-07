abd3b7132af60672c339edd2a0cdd279cba5b5df
Luka Mlinarič <j6.bountyhounter@gmail.com>
2023-05-07 13:07:24 +0200

    Add Search component + styling

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,10 +1,13 @@
 import Navbar from '../navbar/Navbar';
 import Chats from '../chats/Chats';
+import Search from '../search/Search';
+import './Sidebar.scss';
 
 const Sidebar = () => {
   return (
     <div className="sidebar">
       <Navbar />
+      <Search />
       <Chats />
     </div>
   );
