5c7f7fc18e5dfa54c86a64169aaecedc04f01e87
Luka Mlinarič <j6.bountyhounter@gmail.com>
2023-05-08 22:18:10 +0200

    Implement AuthContext

---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,26 @@
+import { createContext, useEffect, useState } from 'react';
+import { auth } from '../services/firebase';
+import { onAuthStateChanged } from 'firebase/auth';
+
+export const AuthContext = createContext();
+
+export const AuthContextProvider = ({ children }) => {
+  const [currentUser, setCurrentUser] = useState({});
+
+  useEffect(() => {
+    const unsub = onAuthStateChanged(auth, (user) => {
+      setCurrentUser(user);
+      console.log(`Current user: ${user.email}`);
+    });
+
+    return () => {
+      unsub();
+    };
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ currentUser }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import App from './components/app/App';
 import './samples/node-api';
 import './index.scss';
+import { AuthContextProvider } from './context/AuthContext';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <AuthContextProvider>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </AuthContextProvider>
 );
 
 postMessage({ payload: 'removeLoading' }, '*');
