98c9f3327158d8d1f3243a468b4053548a7fbae2
AmbicatusThe2nd <rok99tkalcic@gmail.com>
2023-05-08 19:20:03 +0200

    Implemented useState on login too

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,7 +1,18 @@
 import { useNavigate, Link } from 'react-router-dom';
+import { useState } from 'react';
 
 const Login = () => {
   const navigate = useNavigate();
+  const [inputs, setInputs] = useState({
+    email: '',
+    password: '',
+  });
+
+  const handleChange = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setInputs({ ...inputs, [name]: value });
+  };
 
   const handleLogin = (e) => {
     e.preventDefault();
@@ -14,9 +25,19 @@ const Login = () => {
         <div className="formWrapper">
           <span className="logo"> FERI Chat </span>
           <form onSubmit={handleLogin}>
-            <input className="email" placeholder="Email" type="email" />
+            <input
+              className="email"
+              name="email"
+              value={inputs.email}
+              onChange={handleChange}
+              placeholder="Email"
+              type="email"
+            />
             <input
               className="password"
+              name="password"
+              value={inputs.password}
+              onChange={handleChange}
               placeholder="Password"
               type="password"
             />
