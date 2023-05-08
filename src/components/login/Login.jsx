aa2dac31d92e5606e9466f15d6dd3001251886d7
AmbicatusThe2nd <rok99tkalcic@gmail.com>
2023-05-08 19:31:05 +0200

    Implemented login functionality on the component level

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,12 +1,17 @@
 import { useNavigate, Link } from 'react-router-dom';
 import { useState } from 'react';
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import { auth } from '@/services/firebase';
 
 const Login = () => {
   const navigate = useNavigate();
+
   const [inputs, setInputs] = useState({
     email: '',
     password: '',
   });
+
+  const [errors, setErrors] = useState(false);
 
   const handleChange = (e) => {
     const name = e.target.name;
@@ -14,9 +19,14 @@ const Login = () => {
     setInputs({ ...inputs, [name]: value });
   };
 
-  const handleLogin = (e) => {
+  const handleLogin = async (e) => {
     e.preventDefault();
-    navigate('/');
+    try {
+      await signInWithEmailAndPassword(auth, inputs.email, inputs.password);
+      navigate('/');
+    } catch (error) {
+      setErrors(true);
+    }
   };
 
   return (
@@ -41,6 +51,7 @@ const Login = () => {
               placeholder="Password"
               type="password"
             />
+            {errors && <p className="error"> Invalid credentials </p>}
             <button className="loginButton">Login</button>
           </form>
           <p>
