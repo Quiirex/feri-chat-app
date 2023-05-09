fd417e5e9790cdaa0e331631ae3250373535f056
Luka Mlinarič <j6.bountyhounter@gmail.com>
2023-05-09 19:30:38 +0200

    Display a message when no chat is picked

--- src/components/messages/Messages.jsx
+++ src/components/messages/Messages.jsx
@@ -10,20 +10,24 @@ const Messages = () => {
   const { data } = useContext(ChatContext);
 
   useEffect(() => {
-    const unSub = onSnapshot(doc(db, 'chats', data.chatId), (doc) => {
-      doc.exists() && setMessages(doc.data().messages);
-    });
+    if (data.chatId !== 'null') {
+      const unSub = onSnapshot(doc(db, 'chats', data.chatId), (doc) => {
+        doc.exists() && setMessages(doc.data().messages);
+      });
 
-    return () => {
-      unSub();
-    };
+      return () => {
+        unSub();
+      };
+    }
   }, [data.chatId]);
 
   return (
     <div className="messages">
-      {messages.map((m) => (
-        <Message message={m} key={m.id} />
-      ))}
+      {messages.length === 0 || data.chatId === 'null' ? (
+        <div className="noMessages">Pick a person to chat with!</div>
+      ) : (
+        messages.map((m) => <Message message={m} key={m.id} />)
+      )}
     </div>
   );
 };
