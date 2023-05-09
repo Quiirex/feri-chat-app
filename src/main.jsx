4da57b61488b564d1c0a4455258386f3a97309da
Luka Mlinarič <j6.bountyhounter@gmail.com>
2023-05-09 10:02:08 +0200

    Wrap App in ChatContextProvider

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,15 @@ import App from './components/app/App';
 import './samples/node-api';
 import './index.scss';
 import { AuthContextProvider } from './context/AuthContext';
+import { ChatContextProvider } from './context/ChatContext';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <AuthContextProvider>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
+    <ChatContextProvider>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </ChatContextProvider>
   </AuthContextProvider>
 );
 
