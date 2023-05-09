855391564e547d563d8c397cd32ea0e88e56f9b4
Luka Mlinarič <j6.bountyhounter@gmail.com>
2023-05-09 10:07:22 +0200

    Add ChatContext to Chat component

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -2,12 +2,16 @@ import More from '../../assets/more.png';
 import Messages from '../messages/Messages';
 import Input from '../input/Input';
 import './Chat.scss';
+import { ChatContext } from '@/context/ChatContext';
 
 const Chat = () => {
+  const { data } = useContext(ChatContext);
+  console.log(`Chat.jsx: data: ${data}`);
+
   return (
     <div className="chat">
       <div className="chatInfo">
-        <span>Janez Novak</span>
+        <span>{data.user?.displayName}</span>
         <div className="chatIcons">
           <img src={More} alt="" />
           {/* TODO replace with icon */}
