290ecb8279ef672e9d6f7c72aabc0d66781e1e3b
AmbicatusThe2nd <rok99tkalcic@gmail.com>
2023-05-19 23:37:47 +0200

    Implemented dynamic data for hover

--- src/components/message/Message.jsx
+++ src/components/message/Message.jsx
@@ -51,7 +51,13 @@ const Message = ({ message }) => {
         {message.img && <img src={message.img} alt="" />}
         {isHovered && (
           <div id="tooltip" style={{ top: position.y, left: position.x }}>
-            This is a message
+            {message.date.toDate().toLocaleTimeString('en-GB', {
+              year: 'numeric',
+              month: '2-digit',
+              day: '2-digit',
+              hour: '2-digit',
+              minute: '2-digit',
+            })}
           </div>
         )}
       </div>
